4ebf2c3ed06312c08aa1a64cae61cfe8830b6fd6
fedoweb <fedo.web@mail.ru>
2024-09-27 23:13:18 +0300

    change task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,23 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let descriminant = b ** 2 - 4 * a * c;
+
+  if (descriminant === 0) {
+    arr = [(-b / (2 * a))];
+  } else if (descriminant > 0) {
+    arr = [(-b + Math.sqrt(descriminant)) / (2*a), (-b - Math.sqrt(descriminant)) / (2*a)];
+  }
   return arr;
 }
 
+
+
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  const percentMonths = (percent / 100) / 12;
+  const creditBody = amount - contribution;
+  const payMonths = creditBody * (percentMonths + (percentMonths / (((1 + percentMonths) ** countMonths) - 1)));
+  return Number((payMonths * countMonths).toFixed(2));
 }
+
