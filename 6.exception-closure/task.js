31dcdbd4a2e9ad2405a16eddd85e80589b3681f3
fedoweb <fedo.web@mail.ru>
2024-10-18 19:05:10 +0300

    add 6.exception-closure finished version

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,10 +1,10 @@
-﻿//Задание 1
+﻿// Задание 1
 
 function parseCount(value) {
     let parseValue = Number.parseFloat(value);
 
     if (isNaN(parseValue)) {
-        throw new Error("Невалидное значение");
+        throw new Error('Невалидное значение');
     }
 
     return parseValue;
@@ -13,12 +13,13 @@ function parseCount(value) {
 function validateCount(value) {
     try {
         return parseCount(value);
-    } catch(err) {
-        return err;
+        
+    } catch(error) {
+        return error;
     }   
 }
 
-//Задание 2
+// Задание 2
 
 class Triangle {
     constructor(firstSide, secondSide, thirdSide) {
@@ -26,7 +27,7 @@ class Triangle {
             if (!(firstSide + secondSide > thirdSide
                 && firstSide + thirdSide > secondSide
                 && secondSide + thirdSide > firstSide))  {
-                throw new Error("Треугольник с такими сторонами не существует");
+                throw new Error('Треугольник с такими сторонами не существует');
             } 
             
             this.firstSide = firstSide;
@@ -40,8 +41,12 @@ class Triangle {
 
     get area() {
         const halfPerimeter = this.perimeter / 2;
-        return Math.round(Math.sqrt(halfPerimeter * (halfPerimeter - this.firstSide)
-        * (halfPerimeter - this.secondSide) * (halfPerimeter - this.thirdSide)) * 1000) / 1000;
+
+        return Math.round(Math.sqrt(halfPerimeter
+            * (halfPerimeter - this.firstSide)
+            * (halfPerimeter - this.secondSide)
+            * (halfPerimeter - this.thirdSide))
+            * 1000) / 1000;
     }
 }
   
@@ -49,13 +54,13 @@ class Triangle {
 function getTriangle(firstSide, secondSide, thirdSide) {
     try {
         return new Triangle(firstSide, secondSide, thirdSide);
-    } catch (error) {
+    } catch(error) {
         return {
             get perimeter() {
-                return "Ошибка! Треугольник не существует";
+                return 'Ошибка! Треугольник не существует';
               },
               get area() {
-                return "Ошибка! Треугольник не существует";
+                return 'Ошибка! Треугольник не существует';
               },
           };
     }   
